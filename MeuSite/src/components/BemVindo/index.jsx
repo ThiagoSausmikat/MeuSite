8c61726335c90771540914e6da5360d8a80c0b0a
ThiagoSausmikat <tsmoura.93@gmail.com>
2024-08-08 23:22:01 -0300

    feat: lotties agora tem o link/react scroll

--- MeuSite/src/components/BemVindo/index.jsx
+++ MeuSite/src/components/BemVindo/index.jsx
@@ -4,6 +4,9 @@ import paralax from "../../assets/paralax.jpg"
 import Lottie from 'lottie-react';
 import Scroll from "../../assets/scroll.json"
 
+
+import { Link } from 'react-scroll'; // Importa o Link da react-scroll
+
 const BemVindoSection = () => {
    
     return (
@@ -11,7 +14,9 @@ const BemVindoSection = () => {
     <section className={styles.conteiner} id='home'>
       <div className={styles.textContent}>Seja bem vindo ao meu Site</div>
       <div className={styles.lottieContainer}>
+      <Link to="sobre" spy={true} smooth={true} offset={0} duration={600} className={styles.lottieLink}>
       <Lottie animationData={Scroll} loop={true} className={styles.lottie} />
+    </Link>
       </div>
     </section>
 
